b33573e78a83ecf4c0dca112ab4242a3df0b6a73
adri <henintsoa.adri@hotmail.com>
2018-01-03 12:25:11 +0300

    Accueil fini

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/*
+ jQuery;
+ */
+
+"use strict";
+
+jQuery(document).ready(function ($) {
+    $(".loaded").fadeOut();
+    $(".preloader").delay(500).fadeOut(1000);
+    $.toast({
+        text: 'Bonjour, bienvenue sur le site www.dagonline.fr',
+        position: 'top-right',
+        bgColor: '#e1a919',
+        hideAfter: 5000,
+        textColor: 'white',
+        stack: false,
+        loaderBg:'#ffc93e'
+    })
+
+});
+
