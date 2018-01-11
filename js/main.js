fd75ed61717728682e79d10361c405472cdcb170
adri <henintsoa.adri@hotmail.com>
2018-01-12 01:17:31 +0300

    correction 50%

--- js/main.js
+++ js/main.js
@@ -16,6 +16,22 @@ jQuery(document).ready(function ($) {
         stack: false,
         loaderBg:'#ffc93e'
     })
+    var interval;
+    carteAnimate($(".carte-pub div").first());
+    function carteAnimate(selector) {
+        interval = setInterval(function(){
+            if(selector.find("input").first().is("#carte-pub-selector-last")){
+                selector = $(".carte-pub div").first();
+            }
+            $(".carte-pub input").prop("checked",false);
+            selector.find("input").first().prop("checked",true);
+            selector = selector.find("div").first();
+        }, 3000);
+    }
+    $(".carte-pub input").on("click",function () {
+        clearInterval(interval);
+        setTimeout(carteAnimate,1000,$(this).parent());
+    });
 
 });
 
