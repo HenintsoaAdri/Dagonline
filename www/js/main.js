7449cd306412da65292aad153e807b8a0ec89e27
adri <henintsoa.adri@hotmail.com>
2018-02-10 02:26:56 +0300

    root déplacé

---
+++ www/js/main.js
@@ -0,0 +1,47 @@
+/*
+ jQuery;
+ */
+
+"use strict";
+
+jQuery(document).ready(function ($) {
+    $(".loaded").fadeOut();
+    $(".preloader").delay(500).fadeOut(1000);
+    $.toast({
+        text: 'Bonjour, bienvenue sur le site www.dagonline.fr',
+        position: 'top-right',
+        bgColor: '#e1a919',
+        hideAfter: 5000,
+        textColor: 'white',
+        stack: false,
+        loaderBg:'#ffc93e'
+    });
+    $('[data-toggle="tooltip"]').tooltip();
+
+    var interval;
+    carteAnimate($(".carte-pub div").first());
+    function carteAnimate(selector) {
+        interval = setInterval(function(){
+            if(selector.find("input").first().is("#carte-pub-selector-last")){
+                selector = $(".carte-pub div").first();
+            }
+            $(".carte-pub input").prop("checked",false);
+            selector.find("input").first().prop("checked",true);
+            selector = selector.find("div").first();
+        }, 3000);
+    }
+    $(".carte-pub input").on("click",function () {
+        clearInterval(interval);
+        setTimeout(carteAnimate,1000,$(this).parent());
+    });
+
+});
+
+$('.buy').click(function(){
+  $('.bottom').addClass("clicked");
+});
+
+$('.remove').click(function(){
+  $('.bottom').removeClass("clicked");
+});
+
