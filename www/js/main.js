22939fb94164888e685fc2a8aab11eed3ffc9c94
adri <henintsoa.adri@hotmail.com>
2018-02-17 16:07:42 +0300

    Restaurant maj

--- www/js/main.js
+++ www/js/main.js
@@ -17,31 +17,4 @@ jQuery(document).ready(function ($) {
         loaderBg:'#ffc93e'
     });
     $('[data-toggle="tooltip"]').tooltip();
-
-    var interval;
-    carteAnimate($(".carte-pub div").first());
-    function carteAnimate(selector) {
-        interval = setInterval(function(){
-            if(selector.find("input").first().is("#carte-pub-selector-last")){
-                selector = $(".carte-pub div").first();
-            }
-            $(".carte-pub input").prop("checked",false);
-            selector.find("input").first().prop("checked",true);
-            selector = selector.find("div").first();
-        }, 3000);
-    }
-    $(".carte-pub input").on("click",function () {
-        clearInterval(interval);
-        setTimeout(carteAnimate,1000,$(this).parent());
-    });
-
 });
-
-$('.buy').click(function(){
-  $('.bottom').addClass("clicked");
-});
-
-$('.remove').click(function(){
-  $('.bottom').removeClass("clicked");
-});
-
