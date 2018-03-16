48e4e6c0c31ac8666566f3b195af7ff9a4933c9d
antsa <antsuyu@gmail.com>
2018-03-16 15:07:46 +0300

    sante

---
+++ sante/js/main.js
@@ -0,0 +1,23 @@
+/*
+ jQuery;
+ */
+
+"use strict";
+
+jQuery(document).ready(function ($) {
+    $(".loaded").fadeOut();
+    $(".preloader").delay(500).fadeOut(1000);
+    $.toast({
+        text: 'Bonjour, bienvenue sur le site www.dagonline.fr',
+        position: 'top-right',
+        bgColor: '#e1a919',
+        hideAfter: 5000,
+        textColor: 'white',
+        stack: false,
+        loaderBg:'#ffc93e'
+    });
+    $('[data-toggle~="tooltip"]').tooltip({delay:50});
+    try{
+        $(".sticky").stick_in_parent({offset_top:$('.navbar').height() + 10});
+    }catch (e){}
+});
